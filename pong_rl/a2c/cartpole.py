34dcd3a34df699933fb0bc694f158a91d0387ef6
gordonbchen <gordonbchen@gmail.com>
2024-11-13 17:17:24 -0500

    merge actor and critic into model with shared backbone

--- pong_rl/a2c/cartpole.py
+++ pong_rl/a2c/cartpole.py
@@ -16,39 +16,36 @@ from torch.utils.tensorboard import SummaryWriter
 LR = 1e-4
 GAMMA = 0.99
 TRAIN_EPISODES = 2_000
-N_HIDDEN_LAYERS = 4
+N_HIDDEN_LAYERS = 3
 N_NEURONS = 256
 OUTPUT_DIR = Path("outputs/a2c/cartpole")
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
 
-class MLP(nn.Module):
-    """A multi-layer perceptron using ReLU activation."""
+class ActorCritic(nn.Module):
+    """An actor-critic model with a shared backbone."""
 
-    def __init__(self, n_inputs: int, n_outputs: int, n_hidden_layers: int, n_neurons: int) -> None:
+    def __init__(self, n_obs: int, n_actions: int, n_hidden_layers: int, n_neurons: int) -> None:
         """Init actor model."""
         super().__init__()
 
-        in_layer = nn.Sequential(nn.Linear(n_inputs, n_neurons), nn.ReLU())
+        # Shared backbone for actor and critic.
+        in_layer = nn.Sequential(nn.Linear(n_obs, n_neurons), nn.ReLU())
         hidden_layers = nn.Sequential(
-            *[nn.Linear(n_neurons, n_neurons), nn.ReLU()] * (n_hidden_layers - 1)
+            *[nn.Linear(n_neurons, n_neurons), nn.ReLU()] * (n_hidden_layers)
         )
-        out_layer = nn.Linear(n_neurons, n_outputs)
-        self.net = nn.Sequential(in_layer, hidden_layers, out_layer)
+        self.backbone = nn.Sequential(in_layer, hidden_layers)
 
-    def forward(self, xb: torch.Tensor) -> torch.Tensor:
-        """Returns logits."""
-        return self.net(xb)
+        self.actor = nn.Sequential(nn.Linear(n_neurons, n_actions), nn.Softmax(dim=-1))
+        self.critic = nn.Linear(n_neurons, 1)
+
+    def forward(self, xb: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """Returns probability distribution over actions, and value of state."""
+        z = self.backbone(xb)
+        return self.actor(z), self.critic(z)
 
 
-def train(
-    actor: nn.Module,
-    critic: nn.Module,
-    actor_optim: Optimizer,
-    critic_optim: Optimizer,
-    env: gym.Env,
-    train_episodes: int,
-):
+def train(actor_critic: ActorCritic, optim: Optimizer, env: gym.Env, train_episodes: int) -> None:
     """
     Train using A2C.
 
@@ -62,9 +59,7 @@ def train(
     * Optimize policy: loss = -log(probs) * advantage
     * Update value model: loss = advantage ** 2
     """
-    actor.train().to("cuda")
-    critic.train().to("cuda")
-
+    actor_critic.train().to("cuda")
     summary_writer = SummaryWriter(OUTPUT_DIR)
 
     for episode in range(train_episodes):
@@ -73,8 +68,8 @@ def train(
         obs, _ = env.reset()
         state = torch.tensor(obs, dtype=torch.float32, device="cuda").unsqueeze(0)
         while True:
-            # Get action from actor.
-            action_probs = actor(state).squeeze(0)
+            action_probs, curr_value = actor_critic(state)
+            action_probs = action_probs.squeeze(0)
             action = torch.multinomial(action_probs, 1).item()
 
             # Take action.
@@ -84,21 +79,19 @@ def train(
 
             # Calculate advantage.
             with torch.no_grad():
-                td_target = reward + (GAMMA * critic(next_state) * (1 - terminated))
-            advantage = td_target - critic(state)
+                _, next_value = actor_critic(next_state)
+            td_target = reward + (GAMMA * next_value) * (1 - terminated)
+            advantage = td_target - curr_value
 
-            # Optimize critic.
-            critic_loss = advantage**2.0
-            critic_optim.zero_grad()
-            critic_loss.backward()
-            critic_optim.step()
+            # Calculate loss.
+            critic_loss = 0.5 * (advantage**2.0)
+            actor_loss = -1.0 * action_probs[action].log() * advantage.item()
+            loss = critic_loss + actor_loss
 
-            # Optimize actor.
-            log_prob = action_probs[action].log()
-            actor_loss = -1.0 * log_prob * advantage.detach()
-            actor_optim.zero_grad()
-            actor_loss.backward()
-            actor_optim.step()
+            # Optimize.
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
 
             state = next_state
             if terminated or truncated:
@@ -109,9 +102,6 @@ def train(
         summary_writer.add_scalar("reward", episode_reward, episode)
         summary_writer.add_scalar("critic_loss", critic_loss.item(), episode)
         summary_writer.add_scalar("actor_loss", actor_loss.item(), episode)
-        summary_writer.add_scalar("advantage", advantage.item(), episode)
-        summary_writer.add_scalar("log_prob", log_prob.item(), episode)
-        summary_writer.add_scalar("prob", action_probs[action].item(), episode)
 
     summary_writer.close()
 
@@ -121,18 +111,7 @@ if __name__ == "__main__":
     n_obs = env.observation_space.shape[0]
     n_actions = env.action_space.n
 
-    actor = nn.Sequential(
-        MLP(
-            n_inputs=n_obs,
-            n_outputs=n_actions,
-            n_hidden_layers=N_HIDDEN_LAYERS,
-            n_neurons=N_NEURONS,
-        ),
-        nn.Softmax(dim=-1),
-    )
-    critic = MLP(n_inputs=n_obs, n_outputs=1, n_hidden_layers=N_HIDDEN_LAYERS, n_neurons=N_NEURONS)
+    actor_critic = ActorCritic(n_obs, n_actions, N_HIDDEN_LAYERS, N_NEURONS)
+    optim = Adam(actor_critic.parameters(), lr=LR)
 
-    actor_optim = Adam(actor.parameters(), lr=LR)
-    critic_optim = Adam(critic.parameters(), lr=LR)
-
-    train(actor, critic, actor_optim, critic_optim, env, train_episodes=TRAIN_EPISODES)
+    train(actor_critic, optim, env, TRAIN_EPISODES)
